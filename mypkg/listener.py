2ca821e97b94a8ca35344d8321097e8973a6bc2d
Mahiro Yamoto <ma20031031@icloud.com>
2023-12-28 01:36:46 +0900

    a

---
+++ mypkg/listener.py
@@ -0,0 +1,11 @@
+import rcply
+from rcply.node import Node
+from std_msgs.msg import Int16
+
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
+
+rclpy.init()
+node = Node.create_subscription(Int16, "countup", cb, 10)
+rcply.spin(node)
